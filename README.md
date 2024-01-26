d23ec550770ba45541eb7f475c1bdb7ac8e702d0
Dhruv Dave <dhruv1758@gmail.com>
2024-01-26 14:05:42 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+A Python project containing tiny problems and their coding solutions.  
+It's for the sake of practising Python programming.
---
+++ app.py
@@ -0,0 +1,35 @@
+# age = 20
+# print(age)
+# is_online = True
+# name = "John Smith"
+# is_new_patient = False
+# print(name + ' is ' + str(age) + ' years old ');
+# if is_new_patient: print('new ')
+# else: print('old ')
+# print('patient.')
+# new_name = input("What's your name? ")
+# print("Hello, " + new_name)
+# first = float(input('First: '))
+# second = float(input('Second: '))
+# summation = first + second
+# print(str(summation))
+# course = 'Python 4 beginners'
+# # print(course.replace('4', 'for'))
+# print(course.find('4'))
+# print('45' in course)
+# weight = float(input("Weight: "))
+# kg_or_lbs = input("Kg(s) or Lb(s): ").upper()
+# print('Converted weight: ')
+# if kg_or_lbs == 'K':
+#     convertedWeight = (weight * 2.20).__round__(2)
+#     print(str(convertedWeight) + ' lb')
+# elif kg_or_lbs == 'L':
+#     convertedWeight = (weight / 2.20).__round__(2)
+#     print(str(convertedWeight) + ' kg')
+# else:
+#     print('Invalid keyword. Please try again from the start.')
+# print(convertedWeight)
+names = ['Dhruv', 'Vivek', 'Hari']
+print(names[0])
+names.remove('Dhruv')
+print(names)
